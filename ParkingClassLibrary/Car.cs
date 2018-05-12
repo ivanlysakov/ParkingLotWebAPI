b28d017d150ca6c6e37624830f9169c25732170f
Ivan <ivanlysakov@gmail.com>
2018-05-12 23:04:03 +0300

    initial commit

---
+++ ParkingClassLibrary/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ParkingClassLibrary
+{
+    public class Car
+    {
+        public int ID { set; get; } //Свойство идентификатор
+        public int Balance { set; get; }//Свойство баланс
+        public CarTypes TypeofCar { set; get; } //Свойство тип машины
+
+        public static int globalCarID;
+
+        //виды авто
+        public enum CarTypes { Undefined, Passanger, Truck, Bus, Moto }
+        public Car(int balance, CarTypes type)
+
+        {
+            this.ID = Interlocked.Increment(ref globalCarID);
+            this.Balance = balance;
+            this.TypeofCar = type;
+
+
+        }
+    }
+}
